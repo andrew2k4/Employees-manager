61bb63190de06a3497ecd28d8ea98b462a3bf300
andrew <nguemnangandrew@gmail.com>
2024-10-21 00:54:18 +0200

    fix folder structure

---
+++ client/src/app/basic/component/filter-dashboard/filter-dashboard.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-filter-dashboard',
+  templateUrl: './filter-dashboard.component.html',
+  styleUrl: './filter-dashboard.component.scss'
+})
+export class FilterDashboardComponent {
+
+}
