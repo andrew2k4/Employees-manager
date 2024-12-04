af0956427aa783c871d4c6bac101a606325f8790
andrew <nguemnangandrew@gmail.com>
2024-12-04 16:42:04 +0100

    fix connectivity between angular web apps and spring boot

--- server/src/main/java/com/Andrew/service/booking/security/configs/CorsConfig.java
+++ server/src/main/java/com/Andrew/service/booking/security/configs/CorsConfig.java
@@ -1,18 +1,34 @@
 package com.Andrew.service.booking.security.configs;
 
-import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import jakarta.servlet.*;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.stereotype.Component;
 
-@Configuration
-public class CorsConfig implements WebMvcConfigurer {
+
+import java.io.IOException;
+
+@Component
+public class CorsFilter implements Filter {
 
     @Override
-    public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**")
-                .allowedOrigins("http://client:80").allowedOrigins("http://client:80").allowedOrigins("http://localhost:80").allowedOrigins("http://localhost:443").allowedOrigins("*")
-                .allowedMethods("GET", "POST", "PUT", "DELETE")
-                .allowedHeaders("*");
-    }
-}
+    public void doFilter(ServletRequest req, ServletResponse res, FilterChain chain) throws IOException, ServletException {
+        HttpServletRequest request = (HttpServletRequest) req;
+        HttpServletResponse response = (HttpServletResponse) res;
 
+        response.setHeader("Access-Control-Allow-Origin", "*");
+        response.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+        response.setHeader("Access-Control-Allow-Headers", "Authorization, Content-Type");
+        response.setHeader("Access-Control-Allow-Credentials", "true");
+
+        // Si la méthode est OPTIONS, on court-circuite la chaîne et on retourne immédiatement.
+        if ("OPTIONS".equalsIgnoreCase(request.getMethod())) {
+            response.setStatus(HttpServletResponse.SC_OK);
+            return;
+        }
+
+        chain.doFilter(req, res);
+    }
+
+
+}
